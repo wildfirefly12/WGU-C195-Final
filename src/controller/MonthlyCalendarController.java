3b52a939ffd9dbaa48b211652aab9616d3c1534e
Whitney DeCock <wildfirefly12@gmail.com>
2020-07-21 19:41:05 -0600

    Working on calendar

--- src/controller/MonthlyCalendarController.java
+++ src/controller/MonthlyCalendarController.java
@@ -1,4 +1,45 @@
 package controller;
 
-public class MonthlyCalendarController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+
+
+
+public class MonthlyCalendarController implements Initializable {
+
+    @FXML
+    private AnchorPane MonthlyCalendar;
+
+    @FXML
+    private Button PreviousButton;
+
+    @FXML
+    private Button NextButton;
+
+    //create Calendar
+    private static Calendar monthlyCalendar = new Calendar.Builder().build();
+
+    //create day of month fields
+    public static BorderPane setDayPane(int date){
+        BorderPane dayPane = new BorderPane();
+        Label dateLabel = new Label();
+
+        return dayPane;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+
 }
