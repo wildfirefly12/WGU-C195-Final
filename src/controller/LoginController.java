77dba5882b94a96e5433a8696dd7cc5039759b94
wildfirefly12 <wildfirefly12@gmail.com>
2020-08-14 20:49:05 -0600

    working on the weekly view

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -67,7 +67,6 @@ public class LoginController implements Initializable {
                    newWindow.show();
                    Stage currentWindow = (Stage) LoginButton.getScene().getWindow();
                    currentWindow.close();
-
                } catch (IOException error) {
                    error.printStackTrace();
                }
--- src/controller/WeeklyCalendarController.java
+++ src/controller/WeeklyCalendarController.java
@@ -1,16 +1,23 @@
 package controller;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import model.Appointment;
 import util.DBAppointment;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -71,29 +78,74 @@ public class WeeklyCalendarController implements Initializable {
     }
 
     public void addDateLabels(){
+        MonthLabel.setText(new SimpleDateFormat("MMMM").format(calendar.getTime()).toUpperCase());
+
+        while (calendar.get(Calendar.DAY_OF_WEEK) > calendar.getFirstDayOfWeek()) {
+            calendar.add(Calendar.DATE, -1);
+        }
+
         for(int i = 1; i < 8; i++){
-            int date = dayOfMonth;
             Label dateLabel = new Label();
-            dateLabel.setText(Integer.toString(date));
+            dateLabel.setText(Integer.toString(calendar.get(Calendar.DAY_OF_MONTH)));
             dateLabel.setPrefWidth(90);
             dateLabel.setAlignment(Pos.CENTER);
 
             WeeklyCalendar.add(dateLabel, i, 0);
 
-            date++;
+            addAppointments();
+
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
     }
 
     public void addAppointments(){
-        for(int i = 0; i < 7; i++){
-            int dayOfWeek = i + 1;
+        FilteredList<Appointment> appointmentFilteredList = new FilteredList<>(allAppointments, appointment -> appointment.getStart().toLocalDate().isEqual(LocalDateTime.ofInstant(calendar.toInstant(), calendar.getTimeZone().toZoneId()).toLocalDate()));
+        LocalTime hour = LocalTime.MIDNIGHT.plusHours(9);
 
+        for(int i = 1; i < 19; i++){
+            LocalTime finalHour = hour;
+            FilteredList<Appointment> hourlyAppointments = new FilteredList<>(appointmentFilteredList, a -> a.getStart().toLocalTime().equals(finalHour));
+
+            for(Appointment appointment : hourlyAppointments){
+                BackgroundFill gray = new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY);
+                Background appointmentBackground = new Background(gray);
+                Label title = new Label();
+                title.setText(appointment.getTitle());
+                title.setPrefWidth(90);
+                title.setPrefHeight(100);
+                title.setAlignment(Pos.CENTER);
+                title.setBackground(appointmentBackground);
+                int length = appointment.getEnd().getHour() - appointment.getStart().getHour();
+
+                System.out.println(length);
+
+                WeeklyCalendar.add(title, i, calendar.get(Calendar.DAY_OF_WEEK), 1, length);
+            }
+
+            hour = hour.plusMinutes(30);
         }
+    }
+
+    public void weekForward(){
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        WeeklyCalendar.getChildren().clear();
+        addHourLabels();
+        addDateLabels();
+    }
+
+    public void weekBack(){
+        calendar.add(Calendar.DAY_OF_MONTH, -8);
+        WeeklyCalendar.getChildren().clear();
+        addHourLabels();
+        addDateLabels();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
          addHourLabels();
          addDateLabels();
+
+         NextButton.setOnAction(e -> weekForward());
+         PreviousButton.setOnAction(e -> weekBack());
     }
 }
